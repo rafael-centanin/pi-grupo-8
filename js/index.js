666bf2009d17972863b91fa3e4aa87f74edf9aad
Rafael-Centanin <rcentaninmoline@udesa.com>
2025-11-11 19:58:25 -0300

    Arregle las estructuras de los html, cree todos lo js necesarios, y empece con el fetch en el index.js

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const masVendidos = document.querySelector('.mas-vendidos')
+const otrosProductos = document.querySelector('.otros-productos')
+const categoryAll = document.querySelector('.cate-all')
+
+
+fetch('https://dummyjson.com/products')
+.then(function(response) {
+  return response.json()
+})
+.then(function(data) {
+  console.log(data);
+
+  const produ = data.products;
+
+  for (let i = 0; i<10; i++) {
+    const element = produ[i];
+    
+    masVendidos += `
+    <article>
+       <img src="${element.images}" alt="${element.title}">
+       <h2>${element.title}</h2>
+       <p>${element.description}</p>
+       <p>${element.price}</p>
+     </article>
+    `;
+    
+  }
+})
+.catch(function(error) {
+  console.log("Error: " + error);
+})
